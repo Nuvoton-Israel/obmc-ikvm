898cf5b26ee785ba3bf6ffb5de8876547f2c8589
Joseph Liu <kwliu@nuvoton.com>
2023-03-14 11:36:39 +0800

    add kernel version check in create_usbhid.sh
    The udc name has been changed in kernel 6.0 above
    
    Signed-off-by: Joseph Liu <kwliu@nuvoton.com>

--- create_usbhid.sh
+++ create_usbhid.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
 hid_conf_directory="/sys/kernel/config/usb_gadget/obmc_hid"
+
+version_above_5(){
+  MAJOR_VERSION=$(uname -r | awk -F '.' '{print $1}')
+  if [ $MAJOR_VERSION -ge 6 ]  ; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 dev_name="f0830000.udc"
+if version_above_5
+then
+  dev_name="ci_hdrc.0"
+fi
 
 create_hid() {
     # create gadget
